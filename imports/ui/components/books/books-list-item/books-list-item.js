21057a1ca70ee2b769e3d9ec691c2ee05118a3bb
ihorbrmk <paulbrmk@gmail.com>
2017-02-13 21:49:11 +0200

    added UI to books list

--- imports/ui/components/books/books-list-item/books-list-item.js
+++ imports/ui/components/books/books-list-item/books-list-item.js
@@ -1,4 +1,7 @@
 import './books-list-item.html';
 
 Template.Books_listItem.events({
+	'click .books-list-item'(event, instance) {
+		FlowRouter.go(FlowRouter.path('books.details', {id:this._id}));
+	} 
 });
